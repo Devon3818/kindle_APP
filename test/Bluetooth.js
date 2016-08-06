5920ef5b59ddf346ab31743d1e8c823d44bf126b
Devon <k849996781@vip.qq.com>
2016-08-06 11:58:37 +0800

    2016-08-06
    蓝牙功能测试

---
+++ test/Bluetooth.js
@@ -0,0 +1,24 @@
+//初始化蓝牙
+function DV_initialize(){
+	
+}
+
+//扫描蓝牙设备
+function DV_startScan(){
+	
+}
+
+//连接蓝牙设备
+function DV_connect(){
+	
+}
+
+//读取服务和特征
+function DV_discover(){
+	
+}
+
+//设置蓝牙监听
+function DV_subscribe(){
+	
+}
