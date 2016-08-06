0ad4c187fa8b2f325c5442699c8bc516de6985be
Devon <k849996781@vip.qq.com>
2016-08-06 13:12:06 +0800

    2016-08-06

--- test/Bluetooth.js
+++ test/Bluetooth.js
@@ -8,6 +8,11 @@ function DV_startScan(){
 	
 }
 
+//停止扫描设备
+function DV_stopScan(){
+	
+}
+
 //连接蓝牙设备
 function DV_connect(){
 	
@@ -20,5 +25,15 @@ function DV_discover(){
 
 //设置蓝牙监听
 function DV_subscribe(){
+	
+}
+
+//是否初始化适配器
+function DV_isInitialized(){
+	
+}
+
+//适配器是否启用
+function DV_isEnabled(){
 	
 }
