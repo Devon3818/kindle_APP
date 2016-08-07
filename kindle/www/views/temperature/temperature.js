9897149dc6f4e8c883d6cee4eb2498ecd554e7d3
Devon <k849996781@vip.qq.com>
2016-08-07 16:52:48 +0800

    2016-08-07

--- kindle/www/views/temperature/temperature.js
+++ kindle/www/views/temperature/temperature.js
@@ -1,12 +1,7 @@
 angular.module('App')
 	.controller('TemperatureController', function($scope) {
-		$scope.reservation = {
-			checkin: new Date(),
-			checkout: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-			room: 156,
-			rate: 121,
-			wifi: 'resortwifi'
-		};
+		$scope.Resuly = '00.0';
+		$scope.isConnect = 'startScan';
 
 		//初始化蓝牙
 		$scope.ble_initialize = function() {
@@ -17,11 +12,11 @@ angular.module('App')
 					bluetoothle.requestPermission(function(status) {
 
 						if(status["requestPermission"] == true) {
-							alert("requestPermission ok");
+							//alert("requestPermission ok");
 
 							bluetoothle.requestLocation(function(status) {
 								if(status["requestLocation"] == true) {
-									alert("requestLocation ok");
+									//alert("requestLocation ok");
 									$scope.ble_startScan();
 								}
 							}, function() {
@@ -53,13 +48,19 @@ angular.module('App')
 						bluetoothle.stopScan(function(status) {}, function(status) {});
 						var address = status["address"];
 						bluetoothle.connect(function(status) {
-							alert("连接成功");
-							alert(JSON.stringify(status));
+							//alert("连接成功");
+							//alert(JSON.stringify(status));
+
+							$scope.$apply(function() {
+								$scope.isConnect = "Is Connect";
+							});
+
 							bluetoothle.discover(function(status) {
-								alert(JSON.stringify(status));
+								//alert(JSON.stringify(status));
 								bluetoothle.subscribe(function(status) {
 									if(status["status"] == "subscribedResult") {
-										alert(JSON.stringify(status));
+										//alert(JSON.stringify(status));
+										$scope.decoding(status["value"]);
 									}
 								}, function(status) {}, {
 									"address": address,
@@ -96,15 +97,55 @@ angular.module('App')
 
 				//alert(status);
 				if(status["isInitialized"]) {
-					alert("TURES");
 					$scope.ble_initialize();
 				} else {
-					alert("FALSES");
 					$scope.ble_initialize();
 				}
 
 			});
 
+		};
+
+		$scope.decoding = function(value) {
+
+			var bases64 = bases.fromBase(value, '64'),
+				bases16 = bases.toBase(bases64, 16),
+
+				type = bases16.substr(2, 2) + '',
+				num = bases16.substr(0, 2) + '';
+
+			switch(type) {
+				case "00":
+					var str = bases.fromBase(num, '16'),
+						Result = (parseInt(bases.toBase(str, 10))) / 10;
+
+					$scope.$apply(function() {
+						$scope.Resuly = Result;
+					});
+					break;
+				case "10":
+					var first = num.substr(0, 1),
+						result = num.substr(1, 1) + first,
+						str = bases.fromBase(result, '16'),
+						Result = (parseInt(bases.toBase(str, 10)) + 256) / 10;
+
+					$scope.$apply(function() {
+						$scope.Resuly = Result;
+					});
+					break;
+				default:
+					var n = type.substr(1, 1);
+					if(n > 0) {
+						var str = bases.fromBase(num, '16'),
+							Result = (parseInt(bases.toBase(str, 10)) + (256 * n)) / 10;
+
+						$scope.$apply(function() {
+							$scope.Resuly = Result;
+						});
+					}
+			}
+
 		}
+
 		$scope.load();
 	});
